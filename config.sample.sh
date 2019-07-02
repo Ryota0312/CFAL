3ae86576c864df7e4d8e90664bc66b17e5b77eb4
Ryota Nishi <nishi@swlab.cs.okayama-u.ac.jp>
2019-07-02 12:33:55 +0900

    Add comment

--- config.sample.sh
+++ config.sample.sh
@@ -1,3 +1,4 @@
 export YOUR_CFAL_DIR=""        ## example: /Users/yamada/Projecs/CFAL
 export YOUR_HOME_DIR=""        ## example: /Users/yamada
 export YOUR_IGNORE_FILE=()     ## example: ("\/Users\/yamada\/Library\/.*" "\/Users\/yamada\/todo.org")
+                               ## Sorry: This setting do not work now.
