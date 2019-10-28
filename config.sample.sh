efececa0db1289dd5515926b71e5618904e94aee
Ryota Nishi <nishi@swlab.cs.okayama-u.ac.jp>
2019-10-28 16:16:01 +0900

    Fix config.sample.sh

--- config.sample.sh
+++ config.sample.sh
@@ -1,3 +1,3 @@
-export YOUR_CFAL_DIR=""        ## example: /home/yamada/Projecs/CFAL
+export YOUR_CFAL_DIR=""        ## example: /home/yamada/Projects/CFAL
 export YOUR_HOME_DIR=""        ## example: /home/yamada
-export YOUR_IGNORE_LIST=''     ## example: --exclude "/home/yamada/want_ignore/.*"
+export YOUR_IGNORE_LIST=''     ## example: --exclude "/home/yamada/ignore_path/.*" --exclude "/home/yamada/exclude_path/.*
